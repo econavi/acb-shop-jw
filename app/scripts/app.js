3dcc2d47bb30f2b022ef5b7d98c2fff4f5413367
Vladimir Kochenov <kochenov.vl@gmail.com>
2019-10-16 10:21:19 +0300

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,24 @@
+import svg4everybody from 'svg4everybody';
+import mainMenu from '../blocks/main-menu/main-menu';
+
+class Viewer {
+  constructor() {
+    this.mainMenu = mainMenu;
+    this.svg4everybody = svg4everybody;
+    this.init();
+  }
+
+  init() {
+    this.initClasses();
+  }
+
+  initClasses() {
+    this.svg4everybody();
+    this.mainMenu();
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  // eslint-disable-next-line no-new
+  new Viewer();
+});
---
+++ tasks/server.js
@@ -0,0 +1,34 @@
+/* server */
+require('gulp');
+const debuga = require('debuga');
+const browserSync = require('browser-sync').create();
+
+const isDevelopment = !process.env.NODE_ENV
+  || process.env.NODE_ENV === 'development';
+
+module.exports = () => {
+  browserSync.init({
+    files: [
+      'public/**/*',
+    ],
+    server: {
+      baseDir: [
+        'public',
+      ],
+      directory: false,
+      middleware: isDevelopment ? [debuga()] : [],
+    },
+    tunnel: false,
+    reloadOnRestart: true,
+    host: 'localhost',
+    port: 9000,
+    snippetOptions: {
+      rule: {
+        match: /<\/body>/i,
+      },
+    },
+    watchEvents: ['change', 'add', 'unlink', 'addDir', 'unlinkDir'],
+
+  });
+  browserSync.watch('public/**/*.*').on('change', browserSync.reload);
+};

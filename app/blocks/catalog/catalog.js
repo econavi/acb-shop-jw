59f8d5079fb6bc487bf4ee5025ce878d8884c9aa
Vladimir Kochenov <kochenov.vl@gmail.com>
2019-10-22 15:56:19 +0300

    Create catalog view switching

---
+++ app/blocks/catalog/catalog.js
@@ -0,0 +1,47 @@
+const toggleViewCatalog = () => {
+  const options = {
+    selectors: {
+      switcher: 'js-catalog-view-switcher',
+      catalog: 'js-catalog-list',
+      catalogIsList: 'catalog-list--view-h',
+      targetControl: 'view-switcher',
+    },
+    controlNames: {
+      list: 'list',
+      modules: 'modules',
+    },
+  };
+
+  const switcher = document.querySelector(`.${options.selectors.switcher}`);
+  const catalog = document.querySelector(`.${options.selectors.catalog}`);
+
+  // Обработчик переключает css-class каталога
+  // для его отображения плиткой или списком
+  const clickHandler = (e) => {
+    const { target } = e;
+
+    // Если промахнулись мимо кнопок, ничего не делаем
+    const targetName = target.getAttribute('name');
+    if (targetName !== options.selectors.targetControl) return;
+
+    // Получим value нажатой кнопки и состояние отображения каталога
+    const targetValue = target.getAttribute('value');
+    const catalogViewIsList = catalog.classList.contains(options.selectors.catalogIsList);
+
+    // Если текущий вид каталога совпадает со значением нажатой кнопки,
+    // ничего не делаем
+    if (targetValue === options.controlNames.list && catalogViewIsList) return;
+    if (targetValue === options.controlNames.modules && !catalogViewIsList) return;
+
+    // Переключаем класс
+    if (targetValue === options.controlNames.list && !catalogViewIsList) {
+      catalog.classList.add(options.selectors.catalogIsList);
+    } else {
+      catalog.classList.remove(options.selectors.catalogIsList);
+    }
+  };
+
+  switcher.addEventListener('click', clickHandler);
+};
+
+export default toggleViewCatalog;
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,13 @@
 import svg4everybody from 'svg4everybody';
 import mainMenu from '../blocks/main-menu/main-menu';
 import Accordion from '../blocks/accordion/accordion';
+import toggleViewCatalog from '../blocks/catalog/catalog';
 
 class Viewer {
   constructor() {
     this.mainMenu = mainMenu;
     this.svg4everybody = svg4everybody;
+    this.toggleViewCatalog = toggleViewCatalog;
     this.init();
   }
 
@@ -16,6 +18,7 @@ class Viewer {
   initClasses() {
     this.svg4everybody();
     this.mainMenu();
+    this.toggleViewCatalog();
 
     const accordionList = document.querySelectorAll('.accordion');
     [].map.call(accordionList, (e) => new Accordion(e));
